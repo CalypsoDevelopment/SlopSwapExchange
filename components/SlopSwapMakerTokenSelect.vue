0d1fff82299afb601888b59b36ab3451ec89c436
Brandon Fowler <42071238+CalypsoDevelopment@users.noreply.github.com>
2022-05-13 16:01:59 -0700

    Initial Commit

---
+++ components/SlopSwapMakerTokenSelect.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'SlopSwapMakerTokenSelect',
+  components: {},
+  data () {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <SlopSwapLandingPage />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
